b8e4af4b92429872c7f743486711b80d5529d0a8
Jason <jcbryant3419@gmail.com>
2021-05-19 20:18:47 -0500

    Update

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ MongoClient.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, 
     app.use(cors(corsOptions));
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json({ limit: '50mb' }));
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static(path.join(__dirname, 'app/build')));
     
 
 
@@ -773,7 +773,7 @@ MongoClient.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, 
 
     /*GET ROUTES*/
     app.get('/*', function (req, res) {
-      res.sendFile(path.join(__dirname,  'build', 'index.html'));
+      res.sendFile(path.join(__dirname,  'app/build/index.html'));
     });
 
     //Breakfast Route / + Breakfast Tacos
