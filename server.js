6b1a91c8167949680e603f8d395a9c3ca8c018e8
Jason <jcbryant3419@gmail.com>
2021-05-23 15:31:20 -0500

    Update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const dbUrl = process.env.DATABASE_URL;
 const admin = process.env.USER;
 const passKey = process.env.PASSWORD;
 var corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://downsouthrecipes.herokuapp.com',
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
