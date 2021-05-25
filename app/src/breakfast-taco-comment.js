6672f64c3e6fede86404fd4e2b0555733bb515aa
Jason <jcbryant3419@gmail.com>
2021-05-25 17:52:37 -0500

    Updated Action

--- app/src/breakfast-taco-comment.js
+++ app/src/breakfast-taco-comment.js
@@ -87,7 +87,7 @@ export class BreakfastTacoCommentBox extends React.Component {
                 <div className="commentHeader">
                     {flashMessage && <SuccessMessage />}
                     <h5>Leave a Rating!</h5>                   
-                    <form action="/breakfast-tacos" method="POST" className='form'>
+                    <form action="https://downsouthrecipes.herokuapp.com/breakfast-tacos" method="POST" className='form'>
                         <Rater style={{fontSize: '35px'}} interactive={true} total={5} onRate={this.handleRate} rating={this.state.rating} />
                         <br /><br />
                         <hr />
